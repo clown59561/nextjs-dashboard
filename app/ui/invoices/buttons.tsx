05acf943a4a7dc7baf114c6e159f9e279a58e797
정현민 <clown59561@gmail.com>
2024-11-08 12:11:29 +0900

    로그인추가

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,6 +1,8 @@
+'use client';
+
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { deleteInvoice } from '@/app/lib/action';
+import { deleteInvoice } from '@/app/lib/actions';
 
 export function CreateInvoice() {
   return (
@@ -27,8 +29,14 @@ export function UpdateInvoice({ id }: { id: string }) {
 
 export function DeleteInvoice({ id }: { id: string }) {
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    const result = await deleteInvoiceWithId();
+    console.log(result.message);
+  }
+
   return (
-    <form action={deleteInvoiceWithId}>
+    <form action={deleteInvoiceWithId} onSubmit={handleSubmit}>
       <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
